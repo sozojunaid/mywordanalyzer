cbf76ea2b50cee6e854f2f1f0c9e4d14259eb0cc
sozojunaid <jk443345@gmail.com>
2022-08-06 21:37:44 +0500

    Updates

---
+++ precache-manifest.851e50503c74a06bca3f1312d5c829d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5b49cac5ccf08a43056",
+    "url": "/mywordanalyzer/static/css/main.2bd5beab.chunk.css"
+  },
+  {
+    "revision": "e5b49cac5ccf08a43056",
+    "url": "/mywordanalyzer/static/js/main.e5b49cac.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/mywordanalyzer/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "34fa4c687e6484822864",
+    "url": "/mywordanalyzer/static/css/2.fac08d60.chunk.css"
+  },
+  {
+    "revision": "34fa4c687e6484822864",
+    "url": "/mywordanalyzer/static/js/2.34fa4c68.chunk.js"
+  },
+  {
+    "revision": "ac334015d7d85f497c4a",
+    "url": "/mywordanalyzer/static/js/runtime~main.ac334015.js"
+  },
+  {
+    "revision": "ea86058907a2e81ab6142bbf5355f480",
+    "url": "/mywordanalyzer/index.html"
+  }
+];
